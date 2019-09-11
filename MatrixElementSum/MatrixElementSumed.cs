f544e088bf4b6a86d67ff77434fa7868c96f4348
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-11 13:20:55 -0500

    Testing..

---
+++ MatrixElementSum/MatrixElementSumed.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using System;
+
+namespace Matrix
+{
+    public class MatrixElementSumed
+    {
+
+        int[,] rooms = new int[,] { { 0, 1, 1, 2 }, { 0, 5, 0, 0 }, { 2, 0, 3, 3 } };
+
+        [Test]
+        public void ReturnTheFirstRow()
+        {
+            int[,] output = new int[,] { { 0, 1, 1, 2 }, { 0, 5, 0, 0 }, { 2, 0, 3, 3 } };
+
+            Assert.AreEqual(rooms[0, 3], output[0, 3]);
+
+        }
+        
+        [Test]
+        public void GetAllFloorRoomsWithValesSummedUp()
+        {
+            int[,] output = new int[3,4];
+            for(int floor = 0; floor < 3; floor++)
+            {
+                for(int room = 0; room < 4; room++)
+                {
+                     
+                }
+            }
+        }
+        
+        /*
+        [Test]
+        public bool isHauntedRoom()
+        {
+
+            return true;
+        }
+        */
+        
+        
+    }
+}
