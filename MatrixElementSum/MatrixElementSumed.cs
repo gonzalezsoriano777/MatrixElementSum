ba3e29154b21d7fd5264d136b804278fcd6d098f
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2019-09-11 20:57:36 -0500

    Able to print out a result of 7 not my expectedResult. Elements in each floor aren't summed up correctly. Well Fix issue shortly.

--- MatrixElementSum/MatrixElementSumed.cs
+++ MatrixElementSum/MatrixElementSumed.cs
@@ -5,39 +5,39 @@ namespace Matrix
 {
     public class MatrixElementSumed
     {
-
-        int[,] rooms = new int[,] { { 0, 1, 1, 2 }, { 0, 5, 0, 0 }, { 2, 0, 3, 3 } };
+        int[,] matrix = new int[,] { { 0, 1, 1, 2 }, { 0, 5, 0, 0 }, { 2, 0, 3, 3 } };
 
         [Test]
         public void ReturnTheFirstRowsRoom()
         {
-            int[,] output = new int[,] { { 0, 1, 1, 2 }, { 0, 5, 0, 0 } };
+            int[,] output = new int[,] { { 0, 1, 1, 2 } };
 
-            Assert.AreEqual(rooms[0, 3], output[0, 3]);
+            Assert.AreEqual(matrix[0, 3], output[0, 3]);
         }
         
         [Test]
         public void GetAllFloorRoomsWithValesSummedUp()
         {
-            int[,] output = new int[3,4];
-            for(int floor = 0; floor < 3; floor++)
+
+            int roomsSummedUp = 0;
+
+            int expectedOutput = 9;
+
+            for(int floor = 0; floor < matrix.GetLength(0); floor++)
             {
-                for(int room = 0; room < 4; room++)
+                for (int room = 0; room < matrix.GetLength(1); room++)
                 {
-                     
+                    if (matrix[room, floor] != 0)
+                    {
+                        roomsSummedUp += matrix[room, floor];
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
+            Assert.AreEqual(roomsSummedUp, expectedOutput);
         }
-        
-        /*
-        [Test]
-        public bool isHauntedRoom()
-        {
-
-            return true;
-        }
-        */
-        
-        
     }
 }
